32ccae7ebf2f4f04efaf2fce7407966af79e5c07
Leandro Caldeira <leandro_e7@hotmail.com>
2022-12-30 20:46:04 -0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Footer.module.css"
+
+export default function(){
+    return(
+        <footer className={styles.footer}>
+            <p><span>Pokenext</span> &copy; 2022</p>
+        </footer>
+    )
+}
